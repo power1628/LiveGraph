7eef9505acea481e816b88d2dba4934790219fc6
pin.gao <pin.gao2008@gmail.com>
2022-08-24 15:30:28 +0800

    FEAT: devcontainer

---
+++ bench/bench_load.cc
@@ -0,0 +1,18 @@
+
+#include <doctest/doctest.h>
+
+#include <cstdio>
+#include <map>
+#include <mutex>
+#include <random>
+#include <string>
+
+#include <omp.h>
+
+#include "bind/livegraph.hpp"
+#include "core/livegraph.hpp"
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+int main(int argc, char **argv) { return 0; }
